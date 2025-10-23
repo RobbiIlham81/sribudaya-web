ba612f0661103050942aed1ead005779a076bf56
Robbi Amanda Ilham <robbysbg535@gmail.com>
2025-10-23 11:00:50 +0700

    perbaikan pada gambar jpg

--- frontend/src/components/PackageCardPool.jsx
+++ frontend/src/components/PackageCardPool.jsx
@@ -27,7 +27,7 @@ const PackageCardPool = () => {
     },
     {
       id: 3,
-      src: "/images/booking/pool/kolam2.JPG",
+      src: "/images/booking/pool/kolam2.jpg",
       alt: "kolam2.jpg",
       title: "Area Santai Tepi Kolam",
       desc: "Area duduk nyaman bergaya minimalis, ideal untuk bersantai atau menikmati hidangan ringan.",
